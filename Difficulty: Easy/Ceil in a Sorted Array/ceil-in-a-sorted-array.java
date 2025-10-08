1c16e85dbedff471fefc74cc4d9f83915deda57b
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2025-10-08 19:58:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+class Solution {
+    public int findCeil(int[] arr, int x) {
+        // code here
+        int ans = -1;
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+            if(arr[mid]>=x)
+            {
+                ans = mid;
+                e = mid-1;
+            }
+            else 
+            {
+                s = mid+1;
+            }
+        }
+        return ans;
+    }
+}

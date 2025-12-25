3fc31b947b4d5470f8215948f6006114ef3e1c6c
Dev Raj Singh <161117208+DevRajSinghHbtu@users.noreply.github.com>
2025-12-25 23:25:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.java
@@ -0,0 +1,22 @@
+/*
+  class Node {
+   int data;
+    Node next;
+
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    public boolean searchKey(Node head, int key) {
+        // Code here
+        Node node = head;
+        while(node != null)
+        {
+            if(node.data == key)
+            return true;
+            else
+            node = node.next;
+        }
+        return false;
+    }
+}
